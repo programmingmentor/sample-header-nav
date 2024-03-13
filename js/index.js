3bd8a9101b4de22af30d4f8ad454ff7d8df8e753
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-03-13 16:54:10 +0000

    Convert to HTMX

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("partialsLoaded", () => {
+function init() {
   import("./header.js");
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
 });
